030b486b85f0fbba63e3b32876e6c5753f75f9c9
camille <57714750+chipikia040998@users.noreply.github.com>
2021-12-02 20:55:52 +0100

    complétition des fonctions

--- TP_LISTE_ENTIER/intlist.c
+++ TP_LISTE_ENTIER/intlist.c
@@ -220,40 +220,50 @@ void print_lst(struct lst_t * L ) {
 
 struct lst_elm_t * new_lst_elm(int value);
 {
-	//TO DO
+	struct lst_elm_t * Li = (struct lst_elm_t *)calloc(1, sizeof(struct lst_elm_t));
+	Li->x = value;
+	Li->suc = NULL;
+	return Li;
 }
 
 /**************************************************************/
 
 void del_lst_elm(struct lst_elm_t ** ptrE);
 {
-	//TO DO
+	assert(ptrE && *ptrE);
+	free(*ptrE);
+	(*ptrE) = NULL;
 }
 
 /**************************************************************/
 
 int getX ( struct lst_elm_t * E );
 {
-	//TO DO
+	assert(E);
+	return E->x;
 }
 
 /**************************************************************/
 
 struct lst_elm_t * getSuc ( struct lst_elm_t * E );
 {
-	//TO DO
+	assert(E);
+	return E->suc;
 }
 
 /**************************************************************/
 
 void setX (struct lst_elm_t * E, int v );
 {
-	//TO DO
+	assert(E);
+	E->x = v;
 }
 
 /**************************************************************/
 
 void setSuc ( struct lst_elm_t * E, struct lst_elm_t * S );
 {
-	//TO DO
+	assert(E);
+	assert(S);
+	E->suc = S;
 }
