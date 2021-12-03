57f878a2e1ae41d6e6dbc1cfe73504b68452ae2e
chipikia <czell57890@gmail.com>
2021-12-03 13:39:23 +0100

    cration TP matrice

--- TP_LISTE_ENTIER/include/insert.h
+++ TP_LISTE_ENTIER/include/insert.h
@@ -5,8 +5,18 @@
 
 #include <stdbool.h>
 
+/** @brief fonction qui insère après une position une valeur dans une liste
+ * @param L list qui va être modifier
+ * @param value insertion de la valeur
+ * @param place emplacement après lequel on met la valeur
+ */
 void insert_after(struct lst_t * L, const int value, struct lst_elm_t * place);
 
 /******************************************************************************/
 
+/** @brief insère dans l'ordre croissant uen valeur
+ * 
+ * @param L Liste ou on opère les modification
+ * @param value valeur a insérer
+ */
 void insert_ordered(struct lst_t * L, const int value);
